e5b52a88a294085fac91105decc2a88217c4cf3f
ItsXete <[dgiabach2000@gmail.com]>
2025-07-03 23:06:28 -0400

    fixed iss and index

--- iss.js
+++ iss.js
@@ -1,30 +1,33 @@
-const needle = require('needle');
-
+// iss.js
 /**
- * Makes a single API request to retrieve the user's IP address.
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
  * Input:
- *   - A callback (to pass back an error or the IP string)
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
  */
-const fetchMyIP = function(callback) { 
-  const url = 'https://api.ipify.org?format=json';
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
 
-  needle.get(url, (error, response) => {
     if (error) {
       callback(error, null);
       return;
     }
 
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP`), null);
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
       return;
-    }
+    } 
 
-    const ip = response.body.ip;
-    callback(null, ip);
+    const latitude = body.latitude
+    const longitude = body.longitude
+    callback(null, {latitude, longitude});
   });
 };
 
-module.exports = { fetchMyIP };
+// Don't need to export the other function since we are not testing it right now.
+module.exports = { fetchCoordsByIP };
