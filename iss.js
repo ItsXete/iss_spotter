9006353f23e641aa57690500455f9fb6d036326e
ItsXete <[dgiabach2000@gmail.com]>
2025-07-03 23:02:05 -0400

    added iss spotter

---
+++ iss.js
@@ -0,0 +1,30 @@
+const needle = require('needle');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) { 
+  const url = 'https://api.ipify.org?format=json';
+
+  needle.get(url, (error, response) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP`), null);
+      return;
+    }
+
+    const ip = response.body.ip;
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
