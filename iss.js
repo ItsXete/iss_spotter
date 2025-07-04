6de19d267d1165395f4c6ccc7a250fe69985ea08
ItsXete <[dgiabach2000@gmail.com]>
2025-07-03 23:38:53 -0400

    Defining nextISSTimesForMyLocation

--- iss.js
+++ iss.js
@@ -1,15 +1,47 @@
 // iss.js
 
+const needle = require('needle');
+
 /**
- * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Makes a single API request to retrieve the user's IP address.
  * Input:
- *   - An object with keys `latitude` and `longitude`
- *   - A callback (to pass back an error or the array of resulting data)
+ *   - A callback (to pass back an error or the IP string)
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The fly over times as an array of objects (null if error). Example:
- *     [ { risetime: 134564234, duration: 600 }, ... ]
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+      return;
+    }
+
+    const ip = body.ip;
+    callback(null, ip);
+  });
+};
+
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    callback(null, {latitude: body.latitude, longitude: body.longitude});
+  });
+};
+
 const fetchISSFlyOverTimes = function(coords, callback) {
   const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
 
@@ -28,6 +60,3 @@ const fetchISSFlyOverTimes = function(coords, callback) {
     callback(null, passes);
   });
 };
-
-// Don't need to export the other functions since we are not testing them right now.
-module.exports = { fetchISSFlyOverTimes };
